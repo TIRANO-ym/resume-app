3e648b059c96ed29a845adfade68bdacd46ba594
sym8979 <sym8979@gmail.com>
2024-08-26 21:09:59 +0900

    Applied multilingual

--- src/page/main-page/main-page.js
+++ src/page/main-page/main-page.js
@@ -6,20 +6,33 @@ import { FaQuestionCircle } from "react-icons/fa";
 import styled from 'styled-components'
 import { useState } from "react";
 import ToggleButton from '@mui/material/ToggleButton';
+import i18n from '../../language/i18n';
+import { useTranslation } from "react-i18next";
 
 function MainPage() {
   const [darkFlag, setDarkState] = useState(true);
 
-  // const currentInfo = window.location.href;
+  // 카카오 브라우저로 오픈 시 외부 브라우저 사용
   const currentInfo = window.navigator.userAgent.toLowerCase();
   if (currentInfo.indexOf('kakaotalk') !== -1) {
     var target_url = 'http://resume-ym.duckdns.org';
     window.location.href = 'kakaotalk://web/openExternal?url='+encodeURIComponent(target_url);
   }
 
+  // 언어 변경
+  const langList = [
+    { value: "en", name: "English" },
+    { value: "ko", name: "한국어" }
+  ];
+  const [selectedLang, setSelectedLang] = useState("ko");
+  const handleSelectLang = (e) => {
+    setSelectedLang(e.target.value);
+    i18n.changeLanguage(e.target.value);
+  };
+
   return (
     <div className={`main-test ${darkFlag ? 'darkTheme' : 'lightTheme'}`}>
-      <div className="themeToggleButton">
+      <div className="topLine">
         <ToggleButton
           className="toggleSwitch"
           selected={darkFlag}
@@ -29,6 +42,16 @@ function MainPage() {
         >
           {darkFlag ? 'Light Mode' : 'Dark Mode'}
         </ToggleButton>
+        <select
+          className="langSelect"
+          onChange={handleSelectLang} value={selectedLang}
+        >
+          {langList.map((item) => {
+            return <option value={item.value} key={item.value}>
+              {item.name}
+            </option>;
+          })}
+        </select>
       </div>
       <div><ProfileInfo/></div>
       <div><HLine /></div>
@@ -57,13 +80,14 @@ function HLine() {
 }
 
 function ProfileInfo() {
+  const { t } = useTranslation();
   return (
     <div className="profile-info">
       <img className="my-image" src={profile_image}/>
       <div className="description">
-        <div className="my-name">성여명</div>
+        <div className="my-name">{t("profile_info.name")}</div>
         <div className="line" style={{marginTop: "3%"}}>
-          <img src={phone_icon}/> 010-4200-7897
+          <img src={phone_icon}/> {t("profile_info.phone_number")}
         </div>
         <div className="line">
           <img src={main_icon}/>yeomyeong26@gmail.com
@@ -74,23 +98,19 @@ function ProfileInfo() {
 }
 
 function Introduce() {
+  const { t } = useTranslation();
   return (
     <div className="introduce">
       <div className="header">INTRODUCE</div>
       <div className="content">
-      Node.js, TypeScript, JavaScript, HTML, CSS, MySQL 등을 활용한 웹 프론트엔드 및 백엔드 개발에 3년의 경험을 보유하고 있습니다. 실시간 모니터링을 위한 웹 서비스를 개발하는 프로젝트에 참여하면서 복잡한 기능들을 개발한 경험을 가지고 있습니다.
-      <br/><br/>
-      웹에서 기본적으로 지켜야 하는 보안 이슈, 빅데이터를 모니터링하며 생기는 성능 이슈, 각 기능들이 독립적으로 수행 가능하고 사용성/확장성/유지보수성을 고려하여 프론트부터 DB까지 좋은 설계를 하기 위해 고민하는 등 웹 개발자로서 다양한 문제를 접하고 해결해본 경험이 있습니다.
-      <br/>
-      특히 Chart를 활용한 데이터 시각화와 드래그 액션을 통한 화면 커스터마이징 등 복잡한 웹 기능들을 사용자 입장에서는 최대한 쉽고 직관적으로 다가갈 수 있도록 설계하고 개발하는 부분에 자신이 있으며 Component 생명 주기와 Promise 등을 활용한 비동기 처리를 이해하고 적극 활용하여 네트워크 지연으로 인한 오류 발생률을 낮추고 제품 품질을 높이고자 합니다.
-      <br/><br/>
-      팀 리더에게 개발 생산성이 좋고 제품 개선을 위한 설계를 적극적으로 제안하는 팀원으로 인정받은 경험이 있습니다. 다른 팀원이 봉착한 문제를 같이 고민하고 리뷰함으로서 팀이 다 같이 성장하는 팀워크와 커뮤니케이션의 중요성을 잘 알고 실천하기 위해 항상 노력합니다.
+      {t("introduce.content")}
       </div>
     </div>
   );
 }
 
 function Skill() {
+  const { t } = useTranslation();
   return (
     <div className="skill">
       <div className="header">SKILL</div>
@@ -105,7 +125,7 @@ function Skill() {
             <td scope="col">Misc</td>
           </tr>
           <tr>
-            <td>Production<br/>개발 가능 수준</td>
+            <td>{t("skill.production_level")}</td>
             <td>TypeScript<br/>JavaScript<br/>HTML<br/>CSS/SCSS</td>
             <td>Angular<br/>Express<br/>Node.js</td>
             <td>MariaDB<br/>MySQL</td>
@@ -113,7 +133,7 @@ function Skill() {
             <td>Linux<br/>Agile<br/>DevOps<br/>Redux</td>
           </tr>
           <tr>
-            <td>취미 개발 수준</td>
+            <td>{t("skill.hobby_level")}</td>
             <td>Java<br/>Python<br/>C#</td>
             <td>React</td>
             <td>Elasticsearch<br/>MongoDB</td>
@@ -127,6 +147,7 @@ function Skill() {
 }
 
 function WorkExp() {
+  const { t } = useTranslation();
   return (
     <div className="work-exp">
       <div className="header">WORK EXPERIENCE</div>
@@ -134,20 +155,20 @@ function WorkExp() {
         <div className="period">
           2021.02 ~ 2024.02
           <br/>
-          <div className="box">3년 1개월</div>
+          <div className="box">{t("work_exp.3y1m")}</div>
         </div>
         <div className="content">
-          <div className="company-name">(주)윈스</div>
-          <div className="company-role">웹 프론트엔드 및 백엔드 개발자</div>
-          <div>[담당업무]</div>
-            <li>실시간 모니터링을 위한 웹 서비스 프론트엔드 및 백엔드 개발</li>
-            <li>프로젝트 신규 개발 단계부터 출시 후 기능 고도화와 유지보수 수행</li>
+          <div className="company-name">{t("work_exp.company_wins")}</div>
+          <div className="company-role">{t("work_exp.web_front_and_back_developer")}</div>
+          <div>[{t("work_exp.assigned_task")}]</div>
+            <li>{t("work_exp.task1")}</li>
+            <li>{t("work_exp.task2")}</li>
           <br/>
-          <div>[사용기술]</div>
-            <li>웹 Front-end: Angular, TypeScript, HTML, CSS, SCSS</li>
-            <li>웹 Back-end: Express, Node.js, JavaScript, MySQL, MariaDB</li>
-            <li>테스트 코드: Jest</li>
-            <li>버전관리 및 코드리뷰: GitLab</li>
+          <div>[{t("work_exp.use_skill")}]</div>
+            <li>{t("work_exp.web")} Front-end: Angular, TypeScript, HTML, CSS, SCSS</li>
+            <li>{t("work_exp.web")} Back-end: Express, Node.js, JavaScript, MySQL, MariaDB</li>
+            <li>{t("work_exp.test_code")}: Jest</li>
+            <li>{t("work_exp.version_control_and_code_review")}: GitLab</li>
         </div>
       </div>
     </div>
@@ -155,6 +176,7 @@ function WorkExp() {
 }
 
 function Project() {
+  const { t } = useTranslation();
   return (
     <div className="project">
       <div className="header">PROJECT</div>
@@ -163,17 +185,17 @@ function Project() {
           2021.02 ~ 2024.02
         </div>
         <div className="content">
-          <div className="project-name">AI 보안관제 솔루션</div>
-          <div className="project-client">(주)윈스</div>
+          <div className="project-name">{t("project.ai_security_operation_service")}</div>
+          <div className="project-client">{t("work_exp.company_wins")}</div>
 
-          <li>다양한 Chart를 활용한 데이터 시각화를 중심으로 Componet 단위의 위젯을 사용자가 커스터마이징 가능한 대시보드 개발</li>
-          <li>특정 시간대의 트래픽 추이 정보 시각화 기능 개발</li>
-          <li>사용자가 구성한 쿼리 유효성 검사 및 실제 쿼리로 변환 로직 개발</li>
-          <li>패턴 매핑 알고리즘을 적용한 로그 패턴 반자동 분류 기능 개발</li>
-          <li>회원가입, 로그인, 계정 권한 및 2차 SMS 인증 기능 개발</li>
-          <li>JWT 토큰 기반 로그인 세션 관리 프로세스 구축</li>
-          <li>테스트 자동화: 웹 모니터링 성능 테스트를 위한 스크립트 작성 및 공유하여 5000만 종의 이벤트 수집 시 성능 테스트 환경 제공</li>
-          <li>다국어 기반 개발 경험: 감사로그 다국어 적용 개발</li>
+          <li>{t("project.custom_dashboard_command")}</li>
+          <li>{t("project.traffic_monitoring_command")}</li>
+          <li>{t("project.query_logic_command")}</li>
+          <li>{t("project.pattern_semiauto_command")}</li>
+          <li>{t("project.login_mfa_command")}</li>
+          <li>{t("project.jwt_login_session_command")}</li>
+          <li>{t("project.test_automation_command")}</li>
+          <li>{t("project.multilingual_command")}</li>
           <div className="table">
             <table style={{width:"100%"}}>
               <tr>
@@ -184,14 +206,14 @@ function Project() {
                 <td scope="col">DB</td>
               </tr>
               <tr>
-                <td>웹 FE</td>
+                <td>{t("work_exp.web")} FE</td>
                 <td rowspan='2'>Window<br/>Linux</td>
                 <td>Angular</td>
                 <td>TypeScript<br/>HTML<br/>CSS/SCSS</td>
                 <td>-</td>
               </tr>
               <tr>
-                <td>웹 BE</td>
+                <td>{t("work_exp.web")} BE</td>
                 <td>Express<br/>(Node.js)<br/>REST API</td>
                 <td>JavaScript<br/>MySQL<br/>Python</td>
                 <td>MariaDB<br/>MongoDB<br/>ES</td>
@@ -205,6 +227,7 @@ function Project() {
 }
 
 function Education() {
+  const { t } = useTranslation();
   return (
     <div className="education">
       <div className="header">EDUCATION</div>
@@ -213,8 +236,8 @@ function Education() {
           2019.03 ~ 2021.02
         </div>
         <div className="content">
-          <div className="title">한밭대학교</div>
-          <div className="sub-content">컴퓨터공학과 / 졸업</div>
+          <div className="title">{t("education.hanbat_university")}</div>
+          <div className="sub-content">{t("education.cse")} / {t("education.graducation")}</div>
         </div>
       </div>
       <div className="exp">
@@ -222,8 +245,8 @@ function Education() {
           2017.03 ~ 2019.02
         </div>
         <div className="content">
-          <div className="title">서울사이버대학교</div>
-          <div className="sub-content">컴퓨터공학과 / 편입</div>
+          <div className="title">{t("education.seoul_cyber_university")}</div>
+          <div className="sub-content">{t("education.cse")} / {t("education.transfer")}</div>
         </div>
       </div>
       <div className="exp">
@@ -231,8 +254,8 @@ function Education() {
           2014.06 ~ 2017.02
         </div>
         <div className="content">
-          <div className="title">대전공업고등학교</div>
-          <div className="sub-content">반도체학과 / 졸업</div>
+          <div className="title">{t("education.daejeon_high_school")}</div>
+          <div className="sub-content">{t("education.semiconductor_department")} / {t("education.graducation")}</div>
         </div>
       </div>
     </div>
@@ -240,6 +263,7 @@ function Education() {
 }
 
 function License() {
+  const { t } = useTranslation();
   return (
     <div className="education">
       <div className="header">LICENSE</div>
@@ -248,8 +272,8 @@ function License() {
           2020.11
         </div>
         <div className="content">
-          <div className="title">정보처리기사</div>
-          <div className="sub-content">자격증 취득 / 한국산업인력공단</div>
+          <div className="title">{t("license.engineer_information_processing")}</div>
+          <div className="sub-content">{t("license.get_license")} / {t("license.human_resources_development_service_of_korea")}</div>
         </div>
       </div>
       <div className="exp">
@@ -257,8 +281,8 @@ function License() {
           2020.10
         </div>
         <div className="content">
-          <div className="title">제 13회 창의적 종합설계 경진대회</div>
-          <div className="sub-content">동상 수상 / 한밭대학교 공학교육혁신센터</div>
+          <div className="title">{t("license.13th_hanbat_competition")}</div>
+          <div className="sub-content">{t("license.get_bronze_award")} / {t("license.hanbat_center")}</div>
         </div>
       </div>
       <div className="exp">
@@ -266,8 +290,8 @@ function License() {
           2017.01
         </div>
         <div className="content">
-          <div className="title">운전면허1종보통</div>
-          <div className="sub-content">면허증 취득 / 대전지방경찰청</div>
+          <div className="title">{t("license.driver_license")}</div>
+          <div className="sub-content">{t("license.get_driver_license")} / {t("license.daejeon_police")}</div>
         </div>
       </div>
       <div className="exp">
@@ -275,8 +299,8 @@ function License() {
           2016.07
         </div>
         <div className="content">
-          <div className="title">정보처리기능사</div>
-          <div className="sub-content">자격증 취득 / 한국산업인력공단</div>
+          <div className="title">{t("license.craftsman_information_processing")}</div>
+          <div className="sub-content">{t("license.get_license")} / {t("license.human_resources_development_service_of_korea")}</div>
         </div>
       </div>
       <div className="exp">
@@ -284,15 +308,14 @@ function License() {
           2014.11
         </div>
         <div className="content">
-          <div className="title">ITQ한글엑셀 A등급</div>
-          <div className="sub-content">자격증 취득 / 한국생산성본부</div>
+          <div className="title">{t("license.itq_excel_a")}</div>
+          <div className="sub-content">{t("license.get_license")} / {t("license.korea_productivity_center")}</div>
         </div>
       </div>
     </div>
   );
 }
 
-const tooltipMsg = `핵심 경력과 무관한\n기타 프로젝트입니다`;
 const Tooltip = ({ children, message }) => {
   return (
     <Container>
@@ -337,16 +360,17 @@ const Container = styled.div`
 `;
 
 function ETC() {
+  const { t } = useTranslation();
   return (
     <div className="etc">
       <div className="header">OTHERS
-        <Tooltip message={tooltipMsg}>
+        <Tooltip message={t("others.tooltip")}>
           <FaQuestionCircle />
         </Tooltip>
       </div>
       <div className="content">
         <ul>
-          <li>(2020) VR 안전교육 프로그램</li>
+          <li>(2020) {t("others.vr_safty_training_program")}</li>
             <ul>
               <li>Keyword: 
                 <a className="stag">VR</a>
@@ -355,15 +379,15 @@ function ETC() {
               </li>
               <li>GitHub: <a href="https://github.com/TIRANO-ym/Unity-VR-Project">https://github.com/TIRANO-ym/Unity-VR-Project</a></li>
             </ul>
-          <li>(2020) 어떤 분식집의 셀프 주문 시스템</li>
+          <li>(2020) {t("others.self_ordering_system")}</li>
             <ul>
               <li>Keyword: 
-                <a className="stag">소프트웨어 디자인 패턴</a>
+                <a className="stag">{t("others.software_design_pattern")}</a>
                 <a className="stag">Java</a>
               </li>
               <li>GitHub: <a href="https://github.com/TIRANO-ym/Java_SoftwareDesign-Project">https://github.com/TIRANO-ym/Java_SoftwareDesign-Project</a></li>
             </ul>
-          <li>(2019) 모바일 짝맞추기 게임 앱</li>
+          <li>(2019) {t("others.mobile_match_game")}</li>
             <ul>
               <li>Keyword: 
                 <a className="stag">Andriod Studio</a>
@@ -371,7 +395,7 @@ function ETC() {
               </li>
               <li>GitHub: <a href="https://github.com/TIRANOym/Android_Project/tree/master/app/src/main/java/com/tirano/myapplication">https://github.com/TIRANOym/Android_Project/tree/master/app/src/main/java/com/tirano/myapplication</a></li>
             </ul>
-          <li>(2019) 영상 속 특정 인물 추적 프로그램</li>
+          <li>(2019) {t("others.specific_person_tracking_program")}</li>
             <ul>
               <li>Keyword: 
                 <a className="stag">OpenCV</a>
@@ -379,7 +403,7 @@ function ETC() {
               </li>
               <li>GitHub: <a href="https://github.com/TIRANO-ym/Python_VideoProcessing-Project">https://github.com/TIRANO-ym/Python_VideoProcessing-Project</a></li>
             </ul>
-          <li>(2019) 스마트 윈도우</li>
+          <li>(2019) {t("others.smart_window")}</li>
             <ul>
               <li>Keyword: 
                 <a className="stag">Arduino</a>
